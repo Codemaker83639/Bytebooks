060b11f54110bfaf59f09139e04ac0610e77dc7e
Francisco Ogando <Codemaker83639@gmail.com>
2024-11-08 09:27:35 -0400

    Navbar

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app=Flask (__name__)
 def inicio():
     return render_template('Bytebooks/index.html')
 
+@app.route('/Libros')
+def Libros():
+    return render_template('Bytebooks/Libros.html')
+
+@app.route('/Nosotros')
+def Nosotros():
+    return render_template('Bytebooks/Nosotros.html')
+
 if __name__ =='__main__':
     app.run(debug=True)
