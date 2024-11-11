1665d6ffb526cd5aa4729203ded06f15ea9999e1
Francisco Ogando <Codemaker83639@gmail.com>
2024-11-11 11:56:28 -0400

    Hotfix/carpeta_administrativa

--- app.py
+++ app.py
@@ -5,7 +5,31 @@ app=Flask (__name__)
 
 @app.route('/') 
 def inicio():
-    return render_template('Bytebooks/index.html')
+    return render_template('Bytebooks/Index.html')
+
+@app.route('/Libros') 
+def Libros():
+    return render_template('Bytebooks/Libros.html')
+
+@app.route('/Nosotros') 
+def Nosotros():
+    return render_template('Bytebooks/Nosotros.html')
+
+@app.route('/Admin/') 
+def Admin_Index():
+    return render_template('Admin/Index.html')
+
+@app.route('/Login') 
+def Admin_Login():
+    return render_template('Admin/Login.html')
+
+@app.route('/Admin/Libros') 
+def Admin_Libros():
+    return render_template('Admin/Libros.html')
+
+
+
+
 
 if __name__ =='__main__':
     app.run(debug=True)
